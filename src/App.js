f629bc4126d97feabca7e58a44bba15713cde0f3
myduzo <sahli.youssef@outlook.com>
2022-04-22 14:43:36 +0400

    update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import {
+  Flex,
+  SimpleGrid,
+  AspectRatio,
+} from "@chakra-ui/react";
+
+const api = axios.create({
+  baseURL: `https://api.sampleapis.com/futurama/episodes`
+});
 
 function App() {
+  const [episodes, setEpisodes] = useState([]);
+
+  useEffect(() => {
+    const getEpisodes = async () => {
+      try {
+        const res = await api.get('/');
+        setEpisodes(res.data);
+      } catch (error) {
+        console.log("error")
+      }
+    }
+    getEpisodes();
+  }, [])
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        
+        <Flex
+          pt= "5rem"
+          w="full"
+          bg="#F9FAFB"
+          p={10}
+          alignItems="center"
+          justifyContent="center"
         >
-          Learn React
-        </a>
+          <SimpleGrid
+            columns={[1, 2, 3, 4]}
+            gap="24px"
+            mx="auto"
+            textAlign={"left"}
+          >
+            {episodes.slice(0,5).map((item) =>
+              <Card sx={{ maxWidth: 345 }}>
+                <AspectRatio maxW='560px' ratio={1}>
+                  <iframe
+                    title={'video'}
+                    src='https://www.youtube.com/embed/ScMzIvxBSi4'
+                    allowFullScreen
+                  />
+                </AspectRatio>
+                <CardContent >
+                  <Typography gutterBottom variant="h5" component="div">
+                    {item.title}
+                  </Typography>
+                  <Typography gutterBottom variant="body2" color="text.secondary">
+                    {item.originalAirDate}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {item.desc}
+                  </Typography>
+                </CardContent>
+              </Card>
+            )}
+          </SimpleGrid>
+        </Flex>
+
       </header>
     </div>
   );
